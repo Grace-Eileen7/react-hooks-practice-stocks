7036e0d20d1987a9cb79dcc15af02c5ebdc58ba0
Grace Eileen Bass <grace.eilee.bass@gmail.com>
2025-07-26 18:47:19 +0300

    Initial commit: Flatiron Stock Exchange project

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,18 +1,65 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import StockContainer from "./StockContainer";
 import PortfolioContainer from "./PortfolioContainer";
 import SearchBar from "./SearchBar";
 
 function MainContainer() {
+  const [stocks, setStocks] = useState([]);
+  const [portfolio, setPortfolio] = useState([]);
+  const [sortBy, setSortBy] = useState("");
+  const [filterBy, setFilterBy] = useState("All");
+
+  useEffect(() => {
+    fetch("http://localhost:3001/stocks")
+      .then((res) => res.json())
+      .then(setStocks);
+  }, []);
+
+  function handleBuy(stock) {
+    if (!portfolio.includes(stock)) {
+      setPortfolio([...portfolio, stock]);
+    }
+  }
+
+  function handleSell(stock) {
+    setPortfolio(portfolio.filter((s) => s.id !== stock.id));
+  }
+
+  function handleSortChange(value) {
+    setSortBy(value);
+  }
+
+  function handleFilterChange(value) {
+    setFilterBy(value);
+  }
+
+  const sortedStocks = [...stocks].sort((a, b) => {
+    if (sortBy === "Alphabetically") {
+      return a.ticker.localeCompare(b.ticker);
+    } else if (sortBy === "Price") {
+      return a.price - b.price;
+    }
+    return 0;
+  });
+
+  const filteredStocks = sortedStocks.filter((stock) => {
+    return filterBy === "All" || stock.type === filterBy;
+  });
+
   return (
     <div>
-      <SearchBar />
+      <SearchBar
+        sortBy={sortBy}
+        filterBy={filterBy}
+        onSortChange={handleSortChange}
+        onFilterChange={handleFilterChange}
+      />
       <div className="row">
         <div className="col-8">
-          <StockContainer />
+          <StockContainer stocks={filteredStocks} onStockClick={handleBuy} />
         </div>
         <div className="col-4">
-          <PortfolioContainer />
+          <PortfolioContainer portfolio={portfolio} onStockClick={handleSell} />
         </div>
       </div>
     </div>
